f37282187b6d3b7c5e79280f53f735c287b7a81e
Hamdi Yusuf <hyusuf@cs.ualberta.ca>
2019-01-10 12:57:17 -0700

    second commit

--- lab1.py
+++ lab1.py
@@ -5,8 +5,8 @@ import requests
 print (requests.__version__)
 a= requests.get('http://www.google.com')
 print (a.text)
-
-
+b= requests.get('https://raw.githubusercontent.com/hyusuf4/lab1/master/lab1.py')
+print(b.text)
 
 
 
